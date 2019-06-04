758b26c9caa389cabff5fddaaf11d207ee844410
jxramos <jaime.x.ramos@gmail.com>
2019-06-03 22:01:34 -0700

    * cpWURL.js -- bug fix to address the proper document URL now properly retrieved    from the info object according to the onclick parameters described at    https://developer.chrome.com/extensions/contextMenus

--- cpWURL.js
+++ cpWURL.js
@@ -1,13 +1,15 @@
-function appendURL( text , is_no_query_string ) {
-    // TODO find way to get source page's URL not the generated background page
-    var page_url = document.URL + "?abc=def,tuv=xyz"
+function appendURL( info , is_no_query_string ) {
+    var page_url = info.pageUrl + "?abc=def,tuv=xyz"
 
     // Strip off query string
     if ( is_no_query_string ) {
         var qs_idx = page_url.indexOf('?');
         var page_url = page_url.substring(0, qs_idx);
     }
-    return text + "\n" + page_url;
+
+    // Append URL
+    var copyText = info.selectionText + "\n" + page_url;
+    console.log( copyText );
 }
 
 // Copy with URL Only
@@ -15,8 +17,7 @@ chrome.contextMenus.create({
     title: "Copy with URL",
     contexts:["selection"], 
     onclick: function(info, tab) {
-        var copyText = appendURL( info.selectionText , false )
-        console.log( copyText );
+        appendURL( info, false )
     }
 });
 
@@ -25,7 +26,6 @@ chrome.contextMenus.create({
     title: "Copy with URL minus query string",
     contexts:["selection"], 
     onclick: function(info, tab) {
-        var copyText = appendURL( info.selectionText , true )
-        console.log( copyText );
+        appendURL( info, true )
     }
 });
