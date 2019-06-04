879b602f5a2eb77c7d579461a3b403a991ad3590
jxramos <jaime.x.ramos@gmail.com>
2019-06-03 21:15:55 -0700

    * cpWURL.js -- defining 'appendURL' function to stub out the extension's core logic. -- switching to the proper context for context menu's with respect to    text selection. The previous solution only displayed context options    while in the unselected state. This solution works properly with selected    text following the example of https://stackoverflow.com/a/4379141/1330381 -- introducing the URL retrieval logic following the example found at    https://stackoverflow.com/a/1034642/1330381. However this does not quite    do what was expected as the context of this cpWURL.js is to run from a    background page, which seems to be a temporary generated page that is    distinct from the page with the source text.

--- cpWURL.js
+++ cpWURL.js
@@ -1,14 +1,28 @@
-// Create some radio items.
-function radioOnClick(info, tab) {
-    console.log("radio item " + info.menuItemId +
-        " was clicked (previous checked state was " +
-        info.wasChecked + ")");
+function appendURL( text , is_no_query_string ) {
+    // TODO find way to get source page's URL not the generated background page
+    var page_url = document.URL 
+
+    // TODO truncate query string
+
+    return text + "\n" + page_url;
 }
-var radioCopy = chrome.contextMenus.create({
-    "title": "Copy with URL", "type": "radio",
-    "onclick": radioOnClick
+
+// Copy with URL Only
+chrome.contextMenus.create({
+    title: "Copy with URL",
+    contexts:["selection"], 
+    onclick: function(info, tab) {
+        var copyText = appendURL( info.selectionText , false )
+        console.log( copyText );
+    }
 });
-var radioCopyNoQS = chrome.contextMenus.create({
-    "title": "Copy with URL minus query strings", "type": "radio",
-    "onclick": radioOnClick
+
+// Copy with URL minus Query String
+chrome.contextMenus.create({
+    title: "Copy with URL minus query string",
+    contexts:["selection"], 
+    onclick: function(info, tab) {
+        var copyText = appendURL( info.selectionText , true )
+        console.log( copyText );
+    }
 });
