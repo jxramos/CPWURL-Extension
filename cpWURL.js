7c3deacf636ed999b7801d9481c8aadf481913eb
jxramos <jaime.x.ramos@gmail.com>
2019-06-03 22:07:31 -0700

    * cpWURL.js  -- renaming function 'appendURL' to 'copyWithURL' in anticipation of     the subsequent clipboard operation to come.  -- renaming parameter 'is_no_query_string' to 'is_strip_query_string'     to more closely match the operation.  -- enhancing  Append URL comment

--- cpWURL.js
+++ cpWURL.js
@@ -1,13 +1,13 @@
-function appendURL( info , is_no_query_string ) {
+function copyWithURL( info , is_strip_query_string ) {
     var page_url = info.pageUrl + "?abc=def,tuv=xyz"
 
     // Strip off query string
-    if ( is_no_query_string ) {
+    if ( is_strip_query_string ) {
         var qs_idx = page_url.indexOf('?');
         var page_url = page_url.substring(0, qs_idx);
     }
 
-    // Append URL
+    // Append URL to text selection
     var copyText = info.selectionText + "\n" + page_url;
     console.log( copyText );
 }
@@ -17,7 +17,7 @@ chrome.contextMenus.create({
     title: "Copy with URL",
     contexts:["selection"], 
     onclick: function(info, tab) {
-        appendURL( info, false )
+        copyWithURL( info, false )
     }
 });
 
@@ -26,6 +26,6 @@ chrome.contextMenus.create({
     title: "Copy with URL minus query string",
     contexts:["selection"], 
     onclick: function(info, tab) {
-        appendURL( info, true )
+        copyWithURL( info, true )
     }
 });
